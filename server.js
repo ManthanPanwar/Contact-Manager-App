c8902c2107cb3f5c7fea961f222a555e390858a4
ManthanPanwar <manthandpr@gmail.com>
2024-09-07 00:02:24 +0530

    Adding userRoutes, userController, Mongoose Schema for User, User Registration and Password Hashing, JWT access token and user login, verify JWT Token middleware

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const dotenv = require("dotenv").config();
 const contactRoutes = require("./routes/contactRoutes");
+const userRoutes = require("./routes/userRoutes");
 const errorHandler = require("./middleware/errorHandler");
 const connectDb = require("./config/dbConnection");
 // const bodyParser = require("body-parser");
@@ -13,6 +14,7 @@ const port = process.env.PORT || 5000;
 // app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.json());
 app.use("/api/contacts", contactRoutes);
+app.use("/api/users", userRoutes);
 app.use(errorHandler);
 
 app.listen(port, () => {
